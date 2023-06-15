d48b71ffe7cf273db0eaa2001e362b132c177b70
Syed Irfan Faraz <irfanfaraaz03@gmail.com>
2023-06-15 16:31:11 +0530

    Fixed few errors

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -18,30 +18,31 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
     setTimeout(() => setCopied(""), 5000)
   }
 
+  console.log("!!!!!!",post.creator)
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image
-            src={post.creator.image}
+            src={post.creator?.image}
             alt="user_image"
             width={40}
             height={40}
             className="rounded-full object-contain"
           />
           <div className="flex flex-col">
-            <h3 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h3>
-            <p className="font-inter text-sm text-grey-500">{post.creator.email}</p>
+            <h3 className="font-satoshi font-semibold text-gray-900">{post?.creator?.username}</h3>
+            <p className="font-inter text-sm text-grey-500">{post?.creator?.email}</p>
           </div>
         </div>
         <div className='copy_btn' onClick={handleCopy}>
           <Image
             src={
-              copied === post.prompt
+              copied === post?.prompt
                 ? "/assets/icons/tick.svg"
                 : "/assets/icons/copy.svg"
             }
-            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
+            alt={copied === post?.prompt ? "tick_icon" : "copy_icon"}
             width={12}
             height={12}
           />
@@ -55,7 +56,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
       >
         {post.tag}
       </p>
-      {session?.user.id === post.creator._id && pathName === "/profile" && (
+      {session?.user.id === post?.creator?._id && pathName === "/profile" && (
         <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
           <p
             className='font-inter text-sm orange_gradient cursor-pointer'
